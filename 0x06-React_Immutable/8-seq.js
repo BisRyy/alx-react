dcd017a4668334159c2dedd5e318f60eee6d3433
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-03-15 09:50:10 +0300

    Update 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -4,8 +4,12 @@ export default function printBestStudents(inputObject) {
   const newMap = Map(inputObject);
   const lazySeq = Seq(newMap);
   const gradesObject = lazySeq.filter((value) => value.score > 70).map((value) => (
-    value.firstName.charAt(0).toUpperCase()
-    value.lastName.charAt(0).toUpperCase()
+    const fn = value.firstName;
+    const ln = value.lastName;
+
+    value.firstName = fn.charAt(0).toUpperCase() + fn.slice(1);
+    value.lastName = ln.charAt(0).toUpperCase() + ln.slice(1);
+    return null;
   ));
   console.log(gradesObject);
 }
