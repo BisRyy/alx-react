c07bbbadd90e39e20522d12d77ad4ed6fb6a0687
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-03-15 09:25:45 +0300

    Update 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -3,9 +3,9 @@ import { Seq, Map } from 'immutable';
 export default function printBestStudents(inputObject) {
   const newMap = Map(inputObject);
   const lazySeq = Seq(newMap);
-  const gradesObject = lazySeq.filter((value) => value.score >= 70).map((value) => (
-    value.firstName.toUpperCase()
-    value.lastName.toUpperCase()
+  const gradesObject = lazySeq.filter((value) => value.score < 70).map((value) => (
+    value.firstName.charAt(0).toUpperCase()
+    value.lastName.charAt(0).toUpperCase()
   ));
   console.log(gradesObject);
 }
