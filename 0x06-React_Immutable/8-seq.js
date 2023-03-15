a7af0c65def4ce7c7e66102f616a3552d7112b37
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-03-15 09:55:59 +0300

    Fix 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,15 +1,24 @@
-import { Seq, Map } from 'immutable';
+import { Seq } from 'immutable';
 
-export default function printBestStudents(inputObject) {
-  const newMap = Map(inputObject);
-  const lazySeq = Seq(newMap);
-  const gradesObject = lazySeq.filter((value) => value.score > 70).map((value) => (
-    const fn = value.firstName;
-    const ln = value.lastName;
+export default function printBestStudents(object) {
+  const seq = Seq(object);
 
-    value.firstName = fn.charAt(0).toUpperCase() + fn.slice(1);
-    value.lastName = ln.charAt(0).toUpperCase() + ln.slice(1);
-    return null;
-  ));
-  console.log(gradesObject);
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+    return student.score > 70;
+  });
+
+  function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
+  });
+
+  console.log(JSObject);
 }
